4a30fa2a3b17e162c027fb33a727655f184427cd
ermalsalihaj <ermalsalihaj03@gmail.com>
2024-11-14 23:20:07 +0100

    Complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 
 export default function About() {
-  return <div>ABOUT PAGE</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        Rreth Sahand Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Ermal Estate është një agjenci e njohur e pasurive të patundshme që
+        specializohet në ndihmën e klientëve për të blerë, shitur dhe marrë me
+        qira prona në lagjet më të dëshirueshme. Ekipi ynë i agjentëve me
+        përvojë është i përkushtuar për të ofruar shërbim të jashtëzakonshëm dhe
+        për ta bërë procesin e blerjes dhe shitjes sa më të lehtë të jetë e
+        mundur.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Misioni ynë është të ndihmojmë klientët tanë të arrijnë qëllimet e tyre
+        në fushën e pasurive të patundshme duke ofruar këshilla ekspertësh,
+        shërbim të personalizuar dhe një njohje të thellë të tregut lokal.
+        Pavarësisht nëse dëshironi të blini, shisni ose merrni me qira një
+        pronë, ne jemi këtu për t'ju ndihmuar në çdo hap të procesit.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Ekipi ynë i agjentëve ka një përvojë dhe njohuri të gjera në industrinë
+        e pasurive të patundshme, dhe ne jemi të përkushtuar për të ofruar
+        nivelin më të lartë të shërbimit për klientët tanë. Ne besojmë se blerja
+        ose shitja e një prone duhet të jetë një përvojë emocionuese dhe
+        shpërblyese, dhe ne jemi të përkushtuar për ta bërë këtë realitet për
+        secilin nga klientët tanë.
+      </p>
+    </div>
+  );
 }
